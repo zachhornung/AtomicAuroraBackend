c6338fe2556902b041e616258d2d79f0565562dc
Zach Hornung <zachhornung@comcast.net>
2023-12-07 22:01:15 -0800

    added admin filter horizontal to products

--- atomic_aurora_backend/products/admin.py
+++ atomic_aurora_backend/products/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 from atomic_aurora_backend.products.models import Product, ProductColor, ProductKind
 
-# Register your models here.
 
-admin.site.register(Product)
+# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+    filter_horizontal = ["pictures"]
+
+
 admin.site.register(ProductKind)
 admin.site.register(ProductColor)
